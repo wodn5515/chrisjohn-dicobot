9d904d5e1f9f075364f786cb4846f95e3af14d66
Jaewoo Kil <wodn5515@gmail.com>
2025-02-08 23:13:32 +0900

    연결 안끊기기 위해 looping로직 추가

--- apps.py
+++ apps.py
@@ -1,8 +1,10 @@
 import discord
 from discord import app_commands
-from discord.ext import commands
+from discord.ext import commands, tasks
 import os
 from core.market import MarketClient
+from core.spec import SpecClient
+import time
 
 APPLICATION_ID = os.getenv("APPLICATION_ID")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
@@ -30,6 +32,7 @@ class MyBot(commands.Bot):
     async def on_ready(self):
         await bot.tree.sync(guild=discord.Object(id=GUILD_ID))
         print("ready!")
+        await send_message_in_loop.start()
 
         activity = discord.activity.CustomActivity("문의는 크리스존에게")
         await self.change_presence(status=discord.Status.online, activity=activity)
@@ -39,13 +42,37 @@ bot = MyBot()
 
 
 @bot.tree.command(
+    name="거래소", description="거래소 검색", guild=discord.Object(id=GUILD_ID)
+)
+async def market(interaction: discord.Interaction) -> None:
+    # market_categories = []
+    pass
+
+
+@tasks.loop(seconds=10)  # your timer
+async def send_message_in_loop():
+    print("PING")
+    time.sleep(5)
+    print("PONG")
+
+
+@bot.tree.command(
+    name="군장검사", description="군장검사하기", guild=discord.Object(id=GUILD_ID)
+)
+@app_commands.describe(캐릭터명="필수")
+async def spec_check(interaction: discord.Interaction, 캐릭터명: str) -> None:
+    # client = SpecClient(name=캐릭터명)
+    await interaction.response.send_message(content="준비중입니다.", ephemeral=True)
+
+
+@bot.tree.command(
     name="유각", description="유각 가격보기", guild=discord.Object(id=GUILD_ID)
 )
 @app_commands.describe(각인명="미입력시 가격순10개")
 async def test(interaction: discord.Interaction, 각인명: str = None) -> None:
     client = MarketClient()
     client.get_유각(name=각인명)
-    embed = client.get_embed_for_유각()
+    embed = client.get_embed()
     await interaction.response.send_message(embed=embed)
 
 
