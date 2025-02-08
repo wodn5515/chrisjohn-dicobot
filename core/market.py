47c572cca43bd4225814dd9acca0c9f13d79ca12
Jaewoo Kil <wodn5515@gmail.com>
2025-02-08 23:13:13 +0900

    거래소 로직 수정

--- core/market.py
+++ core/market.py
@@ -4,13 +4,6 @@ from discord import Embed
 
 
 class MarketClient(LostarkAPIClient):
-    item: str
-    quality: int
-
-    def __init__(self, item=None, quality=None):
-        self.item = item
-        self.quality = quality
-
     def get_유각(self, name: str):
         path = "/markets/items"
         body = {
@@ -27,21 +20,23 @@ class MarketClient(LostarkAPIClient):
         self._set_data_for_유각(response=response)
 
     def _set_data_for_유각(self, response: Response):
-        data = []
+        data = {"title": "유물 각인서 시세"}
+        items = []
         results = response.json()["Items"]
 
         for result in results:
-            datum = {"name": result["Name"], "price": result["CurrentMinPrice"]}
-            data.append(datum)
+            item = {"name": result["Name"], "price": result["CurrentMinPrice"]}
+            items.append(item)
 
+        data["items"] = items
         self.data = data
 
-    def get_embed_for_유각(self):
-        embed = Embed(title="유물 각인서 시세랭킹", color=0xFF8C00)
+    def get_embed(self):
+        embed = Embed(title=self.data["title"], color=0xFF8C00)
         embed_string = ""
 
-        for datum in self.data:
-            embed_string += f"- {datum['name']} : {int(datum['price']):,}\n"
+        for item in self.data["items"]:
+            embed_string += f"- {item['name']} : {int(item['price']):,}\n"
 
         embed.add_field(name="", value=embed_string)
 
